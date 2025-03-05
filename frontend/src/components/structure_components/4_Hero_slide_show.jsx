d7ab7f0409fb406240165e2b2a0ff0e5828b3f37
bszeidl <szeidl.balazs@hotmail.com>
2025-03-05 23:47:24 +0100

    Logo placed. Main dark and light colors set.

--- frontend/src/components/structure_components/4_Hero_slide_show.jsx
+++ frontend/src/components/structure_components/4_Hero_slide_show.jsx
@@ -3,10 +3,10 @@ import { CCarousel } from '@coreui/react';
 import { CCarouselItem } from '@coreui/react';
 import { CImage } from '@coreui/react';
 import '@coreui/coreui/dist/css/coreui.min.css';
-import wedding from '../images/weddingzoeandalex.png'
-import pizza from '../images/pizza.png';
-import gocsej from '../images/gocsej.png';
-import farkasgym from '../images/farkasgym1.png';
+import clima from '../images/rotenso_klima.webp'
+import external from '../images/rotenso_external.webp';
+import roof from '../images/roof_insulation.jpg';
+
 
 
 const SlideShow = () => {   
@@ -17,20 +17,17 @@ const SlideShow = () => {
         <CCarousel controls indicators className="carousel-container">
 
             <CCarouselItem>
-                <CImage className="d-block w-100" src={wedding} alt="slide 3" />
+                <CImage className="d-block w-100" src={clima} alt="slide 3" />
             </CCarouselItem>
 
             <CCarouselItem>
-                <CImage className="d-block w-100 slide-image" fluid src={gocsej} alt="slide 1"/>     
+                <CImage className="d-block w-100 slide-image" fluid src={roof} alt="slide 1"/>     
             </CCarouselItem>
 
             <CCarouselItem>
-                <CImage className="d-block w-100" src={pizza} alt="slide 2" />
+                <CImage className="d-block w-100" src={external} alt="slide 2" />
             </CCarouselItem>
 
-            <CCarouselItem>
-                <CImage className="d-block w-100" src={farkasgym} alt="slide 3" />
-            </CCarouselItem>
 
         </CCarousel> 
 
